8934e4f688d15652932424b801a1caa2e904aba0
qtcoder999 <30284500+qtcoder999@users.noreply.github.com>
2018-11-17 12:43:09 +0530

    Reset quiz functionality added

--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -3,7 +3,8 @@ import React, {Component} from 'react'
 
 class QuizEnd extends Component{
 
-    handleResetClick(){
+    handleResetClick(e){
+        e.preventDefault();
         this.props.resetClickHandler()
     }
 
