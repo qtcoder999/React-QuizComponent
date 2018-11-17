fa49535495a35ef6f8663f381f89ae544914610c
qtcoder999 <30284500+qtcoder999@users.noreply.github.com>
2018-11-17 11:59:46 +0530

    Reset quiz functionality added

--- src/Quiz.js
+++ src/Quiz.js
@@ -15,7 +15,9 @@ class Quiz extends Component{
         this.setState(state => {
                 return { quiz_position: state.quiz_position + 1 }
         })
-
+    }
+    handleResetClick(){
+        this.setState({ quiz_position: 1 })
     }
 
     render(){
@@ -25,7 +27,7 @@ class Quiz extends Component{
 
             <div>
                 {
-                    isQuizEnd ? <QuizEnd /> :
+                    isQuizEnd ? <QuizEnd resetClickHandler = {this.handleResetClick.bind(this)}/> :
                     <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} 
                     showNextQuestionHandler = {this.showNextQuestion.bind(this)} />
                 }
--- src/QuizEnd.js
+++ src/QuizEnd.js
@@ -3,16 +3,20 @@ import React, {Component} from 'react'
 
 class QuizEnd extends Component{
 
-render(){
+    handleResetClick(){
+        this.props.resetClickHandler()
+    }
 
-    return(
-        <div>
-            <p>Thanks for playing!</p>
-            <a href=''>Reset Quiz</a>
-        </div>
-    )
+    render(){
 
-}
+        return(
+            <div>
+                <p>Thanks for playing!</p>
+                <a href=''>Reset Quiz</a>
+            </div>
+        )
+
+    }
 
 }
 
